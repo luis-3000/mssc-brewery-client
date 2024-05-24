ee2709d9ae9025e3ff7cc2864fab9b6d9632a465
Jose Luis Castillo <josecastillocs@gmail.com>
2024-05-24 18:07:08 -0500

    Implemented a version of Apache HTTP Client Configuration for a much higher RESTful Web Service traffic

---
+++ src/main/java/luis3000/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,49 @@
+package luis3000.msscbreweryclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by jlc on 5/24/2024.
+ * This is for when there's a lot of RESTful Web Service traffic & much higher volume of tests to process,
+ * which will run a lot faster with this implementation. This will really help in the performance of
+ * applications.
+ * This will be the preffered Apache HTTP Client to use going forward.
+ */
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(3000)
+                .setSocketTimeout(3000)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
