b7917c8457034babc42349e0fc08219a3682acb5
Jose Luis Castillo <josecastillocs@gmail.com>
2024-06-07 13:32:16 -0500

    Added Validation and Error Handling

--- src/main/java/luis3000/msscbreweryclient/web/controller/BeerController.java
+++ src/main/java/luis3000/msscbreweryclient/web/controller/BeerController.java
@@ -1,8 +1,9 @@
-package luis3000.msscbreweryclient.web;
+package luis3000.msscbreweryclient.web.controller;
 
 import luis3000.msscbreweryclient.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -22,14 +23,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
 
         // @Todo: provide implementation
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto) {
 
         // @Todo: provide implementation in a future task
         return new ResponseEntity(HttpStatus.NO_CONTENT);
