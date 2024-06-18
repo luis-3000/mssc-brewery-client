14eb54b02f203cfde590573e208ba68ffbb27c17
Jose Luis Castillo <josecastillocs@gmail.com>
2024-06-18 15:35:21 -0500

    Customized a URI

--- src/test/java/luis3000/msscbreweryclient/web/client/BeerControllerTest.java
+++ src/test/java/luis3000/msscbreweryclient/web/client/BeerControllerTest.java
@@ -35,7 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @ExtendWith(RestDocumentationExtension.class)
-@AutoConfigureRestDocs
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "dev.luis3000", uriPort = 80)
 @WebMvcTest(BeerControllerTest.class)
 @ComponentScan(basePackages = "guru.springframework.sfgrestdocsexample.web.mappers")
 class BeerControllerTest {
