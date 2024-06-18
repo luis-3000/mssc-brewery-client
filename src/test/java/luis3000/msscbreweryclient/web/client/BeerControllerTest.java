3aa18b458a92fd8e6a978166296623dccd0c0065
Jose Luis Castillo <josecastillocs@gmail.com>
2024-06-18 16:02:55 -0500

    Performed Documentation Generation

--- src/test/java/luis3000/msscbreweryclient/web/client/BeerControllerTest.java
+++ src/test/java/luis3000/msscbreweryclient/web/client/BeerControllerTest.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 
 //import static org.mockito.ArgumenMatchers.any;
 //import static org.mockito.BDDMochito.given;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+////import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -31,8 +31,8 @@ import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "dev.luis3000", uriPort = 80)
@@ -52,7 +52,7 @@ class BeerControllerTest {
                         .param("iscold", "yes")
                         .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-get",
                         pathParameters(
                                 parameterWithName("beerId").description("UUID of desired beer to get.")
                         ),
@@ -83,7 +83,7 @@ class BeerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoToJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-new",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
